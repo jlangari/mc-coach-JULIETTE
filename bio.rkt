091d87a1463a4772705dcbfb873c6a529f60ee62
jlangari <60789540+jlangari@users.noreply.github.com>
2020-02-12 10:39:26 -0800

    Create bio.rkt

---
+++ bio.rkt
@@ -0,0 +1,18 @@
+#lang mc-coach/bio 
+
+(bio
+ #:name "Juliette Langari"
+ #:profile-pic (triangle 40 'solid 'blue)
+ #:coach-story "Hi there, my name is Juliette Langari and my story actually has nothing to do with coding;
+instead, its about horseback riding and coding my brain. So back when I was about 6 or 7 I was really intrigued
+by horses, my mom decided to sign me up for lessons and as soon as I jumped on the horse I realized I knew nothing.
+For some reason I thought it would just come naturally but I didnt know how to go forward, or stop, or turn.
+It seemed like a new language to me, just like coding. I took years learning and perfecting the sport.
+Granted, I did make a lot of mistakes and that would make me pretty frustrated but after I got the hang of it,
+I found it really rewarding and understood that I had to learn how to learn from my mistakes in order to improve.
+And with that new found ability to learn how to learn, I was able to perform in competitions, and jump over fences, and
+even teach other children how to ride. So with that, I'm here to coach others in the hardwork and dedication it takes
+to learn how to learn and maybe one day I'll make a coding game about horseback riding!")
+
+
+
